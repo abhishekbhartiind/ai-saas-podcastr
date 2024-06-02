a5299286308382ee4c524f95209f3948a45332a7
abhishekbhartiind <imabhishekbharti12@gmail.com>
2024-06-02 12:46:11 +0530

    home layout fixes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcastr</h1>
-        <Button className="bg-orange-1 text-white-1">Create</Button>
+        {/* <Button className="bg-orange-1 text-white-1">Create</Button> */}
       </section>
     </div>
   )
