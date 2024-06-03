8de4b668776149f88bf7bd55e58f4f03c39e06a1
abhishekbhartiind <imabhishekbharti12@gmail.com>
2024-06-03 16:20:36 +0530

    podcastr applc

---
+++ convex/auth.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+// https://profound-fox-96.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://profound-fox-96.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+}
+
+export default authConfig
